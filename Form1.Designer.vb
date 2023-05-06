42034fd3be12a8631c5d921e109c74d6418716c7
Yaksharo <125961401+Yaksharo@users.noreply.github.com>
2023-05-06 18:48:22 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        txtFName = New TextBox()
+        txtMName = New TextBox()
+        Label2 = New Label()
+        txtLName = New TextBox()
+        Label3 = New Label()
+        birthDate = New DateTimePicker()
+        Label4 = New Label()
+        Label5 = New Label()
+        genderSelector = New ComboBox()
+        btnSubmit = New Button()
+        DataGridView1 = New DataGridView()
+        SchoolYear = New Label()
+        btnSubmit1 = New Button()
+        ComboBox3 = New ComboBox()
+        scho = New Label()
+        ComboBox4 = New ComboBox()
+        Label7 = New Label()
+        yearStart = New TextBox()
+        yearEnd = New TextBox()
+        ToolTip1 = New ToolTip(components)
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(86, 92)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(64, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "First Name"
+        ' 
+        ' txtFName
+        ' 
+        txtFName.Location = New Point(86, 110)
+        txtFName.Name = "txtFName"
+        txtFName.Size = New Size(189, 23)
+        txtFName.TabIndex = 1
+        ' 
+        ' txtMName
+        ' 
+        txtMName.Location = New Point(282, 110)
+        txtMName.Name = "txtMName"
+        txtMName.Size = New Size(189, 23)
+        txtMName.TabIndex = 3
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(282, 92)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(79, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Middle Name"
+        ' 
+        ' txtLName
+        ' 
+        txtLName.Location = New Point(476, 110)
+        txtLName.Name = "txtLName"
+        txtLName.Size = New Size(189, 23)
+        txtLName.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(476, 92)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(63, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Last Name"
+        ' 
+        ' birthDate
+        ' 
+        birthDate.Location = New Point(86, 178)
+        birthDate.Name = "birthDate"
+        birthDate.Size = New Size(189, 23)
+        birthDate.TabIndex = 6
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(86, 160)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(55, 15)
+        Label4.TabIndex = 7
+        Label4.Text = "Birthdate"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(282, 160)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(25, 15)
+        Label5.TabIndex = 8
+        Label5.Text = "Sex"
+        ' 
+        ' genderSelector
+        ' 
+        genderSelector.FormattingEnabled = True
+        genderSelector.Items.AddRange(New Object() {"Male", "Female"})
+        genderSelector.Location = New Point(281, 178)
+        genderSelector.Name = "genderSelector"
+        genderSelector.Size = New Size(190, 23)
+        genderSelector.TabIndex = 9
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(282, 209)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(189, 49)
+        btnSubmit.TabIndex = 10
+        btnSubmit.Text = "SUBMIT"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(86, 364)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(579, 231)
+        DataGridView1.TabIndex = 11
+        ' 
+        ' SchoolYear
+        ' 
+        SchoolYear.AutoSize = True
+        SchoolYear.Location = New Point(86, 21)
+        SchoolYear.Name = "SchoolYear"
+        SchoolYear.Size = New Size(64, 15)
+        SchoolYear.TabIndex = 12
+        SchoolYear.Text = "First Name"
+        ' 
+        ' btnSubmit1
+        ' 
+        btnSubmit1.Location = New Point(226, 13)
+        btnSubmit1.Name = "btnSubmit1"
+        btnSubmit1.Size = New Size(189, 49)
+        btnSubmit1.TabIndex = 15
+        btnSubmit1.Text = "SUBMIT"
+        btnSubmit1.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Items.AddRange(New Object() {"Male", "Female"})
+        ComboBox3.Location = New Point(475, 178)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(190, 23)
+        ComboBox3.TabIndex = 17
+        ' 
+        ' scho
+        ' 
+        scho.AutoSize = True
+        scho.Location = New Point(476, 160)
+        scho.Name = "scho"
+        scho.Size = New Size(68, 15)
+        scho.TabIndex = 16
+        scho.Text = "School Year"
+        ' 
+        ' ComboBox4
+        ' 
+        ComboBox4.FormattingEnabled = True
+        ComboBox4.Items.AddRange(New Object() {"Male", "Female"})
+        ComboBox4.Location = New Point(85, 235)
+        ComboBox4.Name = "ComboBox4"
+        ComboBox4.Size = New Size(190, 23)
+        ComboBox4.TabIndex = 19
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(86, 217)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(68, 15)
+        Label7.TabIndex = 18
+        Label7.Text = "Grade Level"
+        ' 
+        ' yearStart
+        ' 
+        yearStart.Location = New Point(86, 39)
+        yearStart.Name = "yearStart"
+        yearStart.Size = New Size(64, 23)
+        yearStart.TabIndex = 20
+        ' 
+        ' yearEnd
+        ' 
+        yearEnd.Location = New Point(156, 39)
+        yearEnd.Name = "yearEnd"
+        yearEnd.Size = New Size(64, 23)
+        yearEnd.TabIndex = 21
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(760, 625)
+        Controls.Add(yearEnd)
+        Controls.Add(yearStart)
+        Controls.Add(ComboBox4)
+        Controls.Add(Label7)
+        Controls.Add(ComboBox3)
+        Controls.Add(scho)
+        Controls.Add(btnSubmit1)
+        Controls.Add(SchoolYear)
+        Controls.Add(DataGridView1)
+        Controls.Add(btnSubmit)
+        Controls.Add(genderSelector)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(birthDate)
+        Controls.Add(txtLName)
+        Controls.Add(Label3)
+        Controls.Add(txtMName)
+        Controls.Add(Label2)
+        Controls.Add(txtFName)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtFName As TextBox
+    Friend WithEvents txtMName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtLName As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents birthDate As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents genderSelector As ComboBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents SchoolYear As Label
+    Friend WithEvents btnSubmit1 As Button
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents scho As Label
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents yearStart As TextBox
+    Friend WithEvents yearEnd As TextBox
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ Form1.vb
@@ -0,0 +1,92 @@
+﻿Imports System.Data.SqlClient
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Private Sub LoadData()
+        'create a connection to SQL Server using connection string
+        Dim con As New SqlConnection("sqlconnectionstring")
+        con.Open()
+        'create a SQL query command to retrieve data from database
+        Dim cmd As New SqlCommand("SELECT LastName + ', ' + FirstName + ' ' + LEFT(COALESCE(MiddleName,''),1) + '.' AS Name, BirthDate, DATEDIFF(year, BirthDate, GETDATE()) AS Age, Sex FROM testingStudentInfo", con)
+        'create an adapter to fill the data from the query command into a DataTable
+        Dim adapter As New SqlDataAdapter(cmd)
+        Dim table As New DataTable()
+        'fill the data into the DataTable
+        adapter.Fill(table)
+        'display the data in the DataGridView and auto-size columns to fill the control
+        DataGridView1.DataSource = table
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        'close the connection to the database
+        con.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadData()
+    End Sub
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        'Connect to database
+        con.ConnectionString = "sqlconnectionstring"
+        'Opens the connection with the database
+        con.Open()
+        'If all the mentioned field below are empty, you put data on them.
+        If (txtFName.Text = "" Or txtLName.Text = "" Or genderSelector.Text = "") Then
+            MessageBox.Show("Please Enter Data at All the Fields")
+            txtFName.Focus()
+        Else
+            'Calculates the age of the student based on the Birth Date and the current system date.
+            Dim selectedDate As Date = birthDate.Value
+            Dim age As Integer = DateDiff(DateInterval.Year, selectedDate, Date.Today)
+
+            'Initializes the insert command.
+            cmd = New SqlCommand("sqlconnectionstring)", con)
+            'Specifies what values are used for the data that will be inserted to the database
+            cmd.Parameters.AddWithValue("@Value1", txtLName.Text)
+            cmd.Parameters.AddWithValue("@Value2", txtFName.Text)
+            cmd.Parameters.AddWithValue("@Value3", txtMName.Text)
+            cmd.Parameters.AddWithValue("@Value4", genderSelector.SelectedItem.ToString())
+            cmd.Parameters.AddWithValue("@Value5", age)
+            cmd.Parameters.AddWithValue("@selectedDate", selectedDate)
+            'Executed the command and uploads them to the database
+            cmd.ExecuteNonQuery()
+            'Message Box is shown if the data is succesfully inserted to the databasse
+            MsgBox("Successfully Enrolled", MsgBoxStyle.Information, "Ok")
+            'Clears all the textbox fields.
+            txtFName.Clear()
+            txtLName.Clear()
+            txtMName.Clear()
+            'Connection to the database is closed.
+            con.Close()
+            ' Refresh the DataGridView to show the new data
+            DataGridView1.DataSource = Nothing ' Clear the current data source
+            DataGridView1.Rows.Clear() ' Clear the existing rows
+            LoadData() ' Load the data again from the database
+        End If
+    End Sub
+
+    Private Sub btnSubmit1_Click(sender As Object, e As EventArgs) Handles btnSubmit1.Click
+        con.ConnectionString = "sqlconnectionstring"
+        con.Open()
+        ' Parse the text values of the yearStart and yearEnd TextBoxes as Short integers.
+        Dim yearStartValue As Short = Short.Parse(yearStart.Text)
+        Dim yearEndValue As Short = Short.Parse(yearEnd.Text)
+        ' Create a new SqlCommand object with a parameterized INSERT statement to insert the yearStart and yearEnd values into the testingSchoolYear table.
+        cmd = New SqlCommand("INSERT INTO testingSchoolYear (YearStart, YearEnd) VALUES (@Value1, @Value2)", con)
+        cmd.Parameters.AddWithValue("@Value1", yearStartValue)
+        cmd.Parameters.AddWithValue("@Value2", yearEndValue)
+        cmd.ExecuteNonQuery()
+        con.Close()
+    End Sub
+
+    Private Sub NoNumbers_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtFName.KeyPress, txtLName.KeyPress, txtMName.KeyPress
+        'Allows alphabetic characters only
+        If Not Char.IsLetter(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+            'Tooltip popup if you press a number
+            ToolTip1.Show("Only alphabetical characters are allowed", CType(sender, Control), 0, -20, 2000)
+        Else
+            ToolTip1.Hide(CType(sender, Control))
+        End If
+    End Sub
+End Class
